1f9eb8bdee0e2ae92183436c2ac5557a495d3c71
Neakhai Yauheni <beonezero@protonmail.com>
2023-04-23 18:13:53 +0300

    h2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Lorem ipsum dolor sit.</p>
+          <h2>1</h2>
+          <h2>2</h2>
+          <h2>3</h2>
+          <h2>4</h2>
+          <h2>5</h2>
         <a
           className="App-link"
           href="https://reactjs-2.org"
