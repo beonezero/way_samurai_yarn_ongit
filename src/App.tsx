c378b3f276f5162c04d99137bed011d70ce5064f
Neakhai Yauheni <beonezero@protonmail.com>
2023-04-23 13:30:50 +0300

    one

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Lorem ipsum dolor sit.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs-2.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Learn React
         </a>
       </header>
     </div>
